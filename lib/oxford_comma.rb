be6631af9389805d9f18c8c9ce4e73f884588630
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-01-29 04:20:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,7 @@ def oxford_comma(array)
   if array.size == 2 
     array.join("and")
   elsif array.size > 2 
-    
+    length = array.size-1
+    string = array[0..length].join(",")
+    string.shovel(",and #{array.last}")
 end
