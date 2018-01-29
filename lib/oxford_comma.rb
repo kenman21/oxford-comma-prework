1a4163385f87130bc8ea76556e011ad2ba28bea2
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-01-29 04:21:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.size == 2 
-    array.join("and")
+    array.join(" and ")
   elsif array.size > 2 
     length = array.size-1
     string = array[0..length].join(",")
-    string.shovel(",and #{array.last}")
+    string.push(",and #{array.last}")
   else 
     array.join()
   end 
